1028f06f06fb8b13dd87da1c4ae34a7f797b165f
riyandrmd <ryanda.rama10@gmail.com>
2023-11-13 11:35:47 +0700

    first commit

---
+++ src/components/Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <>
+            <iframe src="https://docs.google.com/spreadsheets/d/1A2MS3QDKvzFPdJZZSnbU7Tfyr-rSAAIHzucB80vDmBM/edit?usp=sharing?gid=0&amp;single=true&amp;widget=true&amp;headers=false">
+            </iframe>
+        </>
+    )
+}
+
+export default Table
